7bca033d98e71f12cfd7201ec61bc7ff490b6af7
LewieJ08 <lewiej08@gmail.com>
2025-03-10 10:26:59 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name="github_api_cli",
     version="1.0.0",
     packages=find_packages(),  # Automatically finds package directories
-    install_requires=[],  # No external dependencies needed
+    install_requires=[
+        "requests"
+    ],  
     entry_points={  
         "console_scripts": [
             "github-activity=github_api_cli.main:main"  # Links CLI command to main()
@@ -12,7 +14,7 @@ setup(
     },
     author="Lewie Jackson",
     author_email="LewieJ08@gmail.com",
-    description="A simple app that allowing the user to fetch github user infomation",
+    description="A simple app that allows the user to fetch GitHub user information.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/LewieJ08/github_api_cli",  # Add your GitHub repo link once it's created
